747ab310851a1d789ef61db5d88a58b3bf9dd355
jaidev-96787 <jaidevsah007@gmail.com>
2023-02-11 13:02:52 +0530

    Java-Class1

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // What is programming language?
+
+       // => Programming language is a computer language that is used by programmer to communicate with computer.
+       //  It is a set of instructions written in any specific language ( C, C++, Java, Python) to perform a specific
+        //  task.
+
+        // Type of Programming language --
+        //       (1) Low-level programming language.
+        //       (2) High-level programming language.
+
+        // Low-level programming level -> Low-level language is machine-dependent (0s and 1s) programming language.
+        // The processor runs low-level programs directly without the need of a compiler or interpreter, so the
+        // programs written in low-level language can be run very fast.
+        //                  They are two type - Machine language, Assemble language.
+
+        // High-level programming level -> High-level programming language (HLL) is designed for developing
+        // user-friendly software programs and websites. This programming language requires a compiler or
+        // interpreter to translate the program into machine language (execute the program).
+
+        // What is compiler ?
+
+        // => A compiler in Java is a software program that takes the source code written in Java programming
+        // language and converts it into machine-readable code, which is then executed on a computer. The
+        // compiled code runs faster than the source code because it's in a format that can be executed directly
+        // by the computer's processor
+        //    In summary, the compiler in Java is responsible for taking the human-readable Java source code and
+        //    transforming it into the machine-executable code that can be run on a computer.
+
+        // What is Java ?
+
+        // =>  Java is a high-level programming language developed by Sun Microsystems (now owned by Oracle Corporation)
+        // in the mid-1990s. It is an object-oriented language that is designed to be portable across different
+        // platforms, which means that Java programs written on one platform can run on another platform without
+        // modification. Java is commonly used to build standalone applications, web applications, and mobile apps,
+        // as well as being a popular choice for developing large-scale enterprise applications. Java is also used
+        // in scientific computing, financial applications, and games development, among other areas.
+
+        // Advantage of Java -
+        //1.  Platform independent: Java is designed to run on any device that has a Java Virtual Machine (JVM)
+        // installed, making it highly portable. This means that Java programs can run on any operating system,
+        // including Windows, macOS, and Linux.
+        //
+        //2. Object-Oriented: Java is a pure object-oriented programming language, which means that all code written in
+        // Java is organized into objects, making it easier to write, maintain, and understand.
+        //
+        //3. Rich API: Java has a large and comprehensive API, which provides a wide range of functionality,
+        // including I/O, networking, and XML parsing.
+        //
+        //4. Secure: Java is designed with security in mind, making it a good choice for developing applications that
+        // handle sensitive information. Java's security features include automatic memory management and secure
+        // class loading.
+        //
+        //5. Scalability: Java is highly scalable, making it suitable for large-scale enterprise applications. Java's
+        // scalability is due to its support for multithreading, which allows multiple tasks to be executed
+        // simultaneously.
+        //
+        //6. Wide Adoption: Java is widely used by organizations, developers, and students, which means that there is a
+        // large community of developers who can provide support and resources, as well as a large pool of job
+        // opportunities for Java developers.
+
+        //Introduction of java
+
+        // Java is a high-level, general-purpose programming language that was first released in 1995 by Sun
+        // Microsystems (now owned by Oracle Corporation). It is an object-oriented language that is designed
+        // to be portable and platform-independent, which means that Java programs can run on any device that
+        // has a Java Virtual Machine (JVM) installed, regardless of the underlying hardware and operating
+        // system. Java is used to develop a wide range of applications, including standalone applications,
+        // web applications, mobile apps, games, and enterprise software. It has a large and comprehensive API,
+        // which provides a wide range of functionality, including I/O, networking, and XML parsing. Java is also
+        // known for its security features, which make it a good choice for developing applications that handle
+        // sensitive information. With its wide adoption, Java has a large and active community of developers,
+        // which provides support and resources for learning and using the language.
+
+        // What is Compile error ?
+
+        // => While writing the code, if some code is not declared then it through compile error.
+
+        // What is Syntax error ?
+
+        // => While writing code in java, if we follow all the rule which is binded by the java company then its
+        // would not through syntax error but if would not follow the rule then it through syntax error.
+
+        // JRE - JDK are physically present in the form of program which is help us to run different-different
+        // program.
+
+        // JDK - Any thing that help us to run our program is JDK.
+        // JRE - And the place where we are running is JRE.
+        // JVM - And the JDK and JRE together are JVM.
+
+    }
+}
